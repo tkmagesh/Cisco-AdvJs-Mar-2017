7dd20680b263e8333ba16892a63d57620bcbd551
Magesh <tkmagesh77@gmail.com>
2017-03-23 15:29:32 +0530

    Calculator Solution & Click Tracker Seed

--- Day-01/01-Language/src/calculator.js
+++ Day-01/01-Language/src/calculator.js
@@ -1,3 +1,34 @@
 function add(x,y){
-	return x + y;
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+
+/*
+function add(...args){
+	function parseArg(n){
+		if (Array.isArray(n)) return add(...n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return args.length <= 1 ? parseArg(args[0]) : parseArg(args[0]) + add(args.slice(1));
+}
+*/
+
+//Function Invocation Patterns
+/*
+1. As a method of an object (obj)
+	this -> obj
+
+2. As a function
+	this -> global (window) 
+
+3. Using the "call()" method 
+	this -> passed as an argument
+
+4. Using the "apply()" method
+	this -> passed as an argument
+*/
