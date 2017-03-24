d4a7869bb435cdeedecf9e8f3609256004675ed5
Magesh <tkmagesh77@gmail.com>
2017-03-24 11:45:56 +0530

    groupBy Implementation

--- Day-02/03-FP/products.js
+++ Day-02/03-FP/products.js
@@ -14,8 +14,8 @@ function describe(title, fn){
 /*
 sort
 filter
-all
-any
+all - assignment
+any - assignment
 groupBy
 map
 aggregate
@@ -151,4 +151,38 @@ describe('filter', function(){
 			});
 		});
 	})
+});
+
+describe('groupBy', function(){
+	function groupBy(list, keySelector){
+		var result = {};
+		for(var index = 0; index < list.length; index++){
+			var key = keySelector(list[index]);
+			result[key] = result[key] || [];
+			result[key].push(list[index]);
+		}
+		return result;
+	}
+	function printGroup(groupedItem){
+		for(var key in groupedItem){
+			describe('Key - [' + key + ']', function(){
+				console.table(groupedItem[key]);
+			});
+		}
+	}
+	describe("Products by catgory", function(){
+		var categoryKeySelector = function(product){
+			return product.category;
+		};
+		var productsByCategory = groupBy(products, categoryKeySelector);
+		printGroup(productsByCategory);
+	});
+
+	describe("Products by cost", function(){
+		var costKeySelector = function(product){
+			return product.cost < 50 ? "affordable" : "costly";
+		};
+		var productsByCost = groupBy(products, costKeySelector);
+		printGroup(productsByCost);
+	});
 });
